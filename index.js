3131e820a206234452bf0cdd8b140ed1a47d6fca
synnic <synnic@live.hk>
2019-04-25 17:28:29 +0800

    v4

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let currentPath = [];
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     background(255);
+    document.addEventListener('touchmove',preventDefault, false);
+    window.addEventListener('DOMMouseScroll', wheel, false);
 }
 
 function draw() {
@@ -47,10 +49,48 @@ var saveButton = document.querySelector('#saveAs');
 
 saveButton.addEventListener('click',function(e){
     e.preventDefault();
-    console.log('hihi');
     save();
 
 })
+
+function wheel(e) {
+    preventDefault(e);
+  }
+
+// DESKTOP
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+// left: 37, up: 38, right: 39, down: 40,
+// (Source: http://stackoverflow.com/a/4770179)
+
+//   var keys = [32,33,34,35,36,37,38,39,40];
+//   function keydown(e) {
+//     for (var i = keys.length; i--;) {
+//         if (e.keyCode === keys[i]) {
+//             preventDefault(e);
+//             return;
+//         }
+//     }
+// }
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;  
+  }
+// function disable_scroll() {
+//     if (window.addEventListener) {
+//         window.addEventListener('DOMMouseScroll', wheel, false);
+//     }
+//     window.onmousewheel = document.onmousewheel = wheel;
+//     document.onkeydown = keydown;
+//     disable_scroll_mobile();
+//   }
+  
+// function disable_scroll_mobile(){
+//     document.addEventListener('touchmove',preventDefault, false);
+//   }
+
 
 //window.addEventListener('load',() =>
 //{
