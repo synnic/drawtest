42d6e6c9b1ee5a7554c0b495a0f8c4180af6501f
synnic <synnic@live.hk>
2019-04-30 17:51:12 +0800

    v1

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const formidable = require('formidable');
-const fs = require('fs');
-const path = require('path');
 var bodyParser = require('body-parser');
 const { Storage } = require('@google-cloud/storage');
-const Multer = require('multer');
 const vision = require('@google-cloud/vision');
 
-const CLOUD_BUCKET = process.env.GCLOUD_STORAGE_BUCKET || 'drawtest-238706.appspot.com';
-const PROJECT_ID = process.env.PROJECT_ID || 'drawtest-238706';
-const KEY_FILE = process.env.GCLOUD_KEY_FILE || 'drawtest-f391640b1504.json';
+const CLOUD_BUCKET = process.env.GCLOUD_STORAGE_BUCKET || 'pigturechallenge.appspot.com';
+const PROJECT_ID = process.env.PROJECT_ID || 'pigturechallenge';
+const KEY_FILE = process.env.GCLOUD_KEY_FILE || 'pigturechallenge-9ae039f98d06.json';
 const PORT = process.env.PORT || 8080;
 var imageTest;
 
@@ -26,20 +22,6 @@ const client = new vision.ImageAnnotatorClient(
 
 
 async function checkVision(ImagePublicURL) {
-   // Creates a client
-
-// Performs label detection on the image file
-// client
-//   .labelDetection(ImagePublicURL)
-//   .then(results => {
-//     const labels = results[0].labelAnnotations;
-
-//     console.log('Labels:');
-//     labels.forEach(label => console.log(label.description));
-//   })
-//   .catch(err => {
-//     console.error('ERROR:', err);
-//   });
   const [objResult] = await client.objectLocalization(ImagePublicURL);
   const objects = objResult.localizedObjectAnnotations;
   var resultString = '';
@@ -54,13 +36,6 @@ async function checkVision(ImagePublicURL) {
 
 
 const bucket = storage.bucket(CLOUD_BUCKET);
-
-const multer = Multer({
-  storage: Multer.MemoryStorage,
-  limits: {
-    fileSize: 2 * 1024 * 1024 // no larger than 5mb
-  }
-});
 
 const app = express();
 
@@ -98,24 +73,12 @@ app.get('/', async (req, res) => {
 
 });
 
-// app.get("/gupload", (req, res) => {
-//   res.sendFile(path.join(`${__dirname}/index.html`));
-// });
-// var upload = multer({dest:'/pupload'})
-// // Process the file upload and upload to Google Cloud Storage.
 
 app.post("/", function(req, res) {
     imageTest = req.body.imageURL.split(';base64,').pop();
-//     req.file = fs.writeFile('image123.png', imageTest, {encoding: 'base64'}, function(err) {
-//     console.log('File created');
-// });
-  // Create a new blob in the bucket and upload the file data.
   var timestamp = new Date();
   var newName = 'drawing'+timestamp.toISOString().replace(/T/, '').replace(/\..+/, '').replace(/-/,'').replace(/:/,'').replace(/-/,'').replace(/:/,'');
   const blob = bucket.file(newName);
-
-  // Make sure to set the contentType metadata for the browser to be able
-  // to render the image instead of downloading the file (default behavior)
   const blobStream = blob.createWriteStream({
     metadata: {
       contentType: 'image/jpeg',
@@ -132,8 +95,6 @@ app.post("/", function(req, res) {
 
     // Make the image public to the web (since we'll be displaying it in browser)
     blob.makePublic().then(() => {
-      //res.status(200).send(`Success!\n Image uploaded to ${publicUrl}`);
-    //  res.status(200).redirect('result.html');
      checkVision(publicUrl).then(x=>{
         var htmlContent = 
         `<div>
@@ -148,9 +109,6 @@ app.post("/", function(req, res) {
   });
 
   blobStream.end(Buffer.from(imageTest, 'base64'));
-
-//Get the image from storage
-//Check vision
 });
 
 // Start the server
